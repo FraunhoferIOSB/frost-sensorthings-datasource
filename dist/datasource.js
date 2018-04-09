b57802301d591e28e7c22f1fe5729e8cf5d81ee8
rubanraj54 <rubanraj54@gmail.com>
2018-04-09 15:31:46 +0200

    Filter targets that are set to hidden

--- dist/datasource.js
+++ dist/datasource.js
@@ -1,202 +1,208 @@
 "use strict";
 
 System.register(["lodash", "moment"], function (_export, _context) {
-  "use strict";
+    "use strict";
 
-  var _, moment, _createClass, GenericDatasource;
+    var _, moment, _createClass, GenericDatasource;
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
     }
-  }
 
-  return {
-    setters: [function (_lodash) {
-      _ = _lodash.default;
-    }, function (_moment) {
-      moment = _moment.default;
-    }],
-    execute: function () {
-      _createClass = function () {
-        function defineProperties(target, props) {
-          for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];
-            descriptor.enumerable = descriptor.enumerable || false;
-            descriptor.configurable = true;
-            if ("value" in descriptor) descriptor.writable = true;
-            Object.defineProperty(target, descriptor.key, descriptor);
-          }
-        }
+    return {
+        setters: [function (_lodash) {
+            _ = _lodash.default;
+        }, function (_moment) {
+            moment = _moment.default;
+        }],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
 
-        return function (Constructor, protoProps, staticProps) {
-          if (protoProps) defineProperties(Constructor.prototype, protoProps);
-          if (staticProps) defineProperties(Constructor, staticProps);
-          return Constructor;
-        };
-      }();
-
-      _export("GenericDatasource", GenericDatasource = function () {
-        function GenericDatasource(instanceSettings, $q, backendSrv, templateSrv) {
-          _classCallCheck(this, GenericDatasource);
-
-          this.type = instanceSettings.type;
-          this.url = instanceSettings.url;
-          this.name = instanceSettings.name;
-          this.q = $q;
-          this.backendSrv = backendSrv;
-          this.templateSrv = templateSrv;
-          this.withCredentials = instanceSettings.withCredentials;
-          this.headers = { 'Content-Type': 'application/json' };
-          if (typeof instanceSettings.basicAuth === 'string' && instanceSettings.basicAuth.length > 0) {
-            this.headers['Authorization'] = instanceSettings.basicAuth;
-          }
-        }
-
-        _createClass(GenericDatasource, [{
-          key: "getTimeFilter",
-          value: function getTimeFilter(options) {
-            var from = options.range.from.format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z";
-            var to = options.range.to.format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z";
-            return "phenomenonTime gt " + from + " and phenomenonTime lt " + to;
-          }
-        }, {
-          key: "sleep",
-          value: function sleep(delay) {
-            var start = new Date().getTime();
-            while (new Date().getTime() < start + delay) {};
-          }
-        }, {
-          key: "query",
-          value: function query(options) {
-            var allPromises = [];
-            var allTargetResults = { data: [] };
-            var self = this;
-            var timeFilter = this.getTimeFilter(options);
-
-            // /Datastreams(16)/Observations?$filter=phenomenonTime%20gt%202018-03-14T16:00:12.749Z%20and%20phenomenonTime%20lt%202018-03-14T17:00:12.749Z&$select=result,phenomenonTime
-
-            _.forEach(options.targets, function (target) {
-              allPromises.push(this.doRequest({
-                url: this.url + '/Datastreams(' + target.datastreamID + ')/Observations?' + '$filter=' + timeFilter,
-                // data: query,
-                method: 'GET'
-              }).then(function (response) {
-                var filtered = _.map(response.data.value, function (value, index) {
-                  return [value.result, moment(value.resultTime, "YYYY-MM-DDTHH:mm:ss.SSSZ").format('x')];
-                });
-                return {
-                  'target': target.dsTarget.toString(),
-                  'datapoints': filtered
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
                 };
-              }));
-            }.bind(this));
+            }();
 
-            return Promise.all(allPromises).then(function (values) {
-              _.forEach(values, function (value) {
-                allTargetResults.data.push(value);
-              });
-              return allTargetResults;
-            });
-          }
-        }, {
-          key: "testDatasource",
-          value: function testDatasource() {
-            return this.doRequest({
-              url: this.url,
-              method: 'GET'
-            }).then(function (response) {
-              if (response.status === 200) {
-                return { status: "success", message: "Data source is working", title: "Success" };
-              }
-            });
-          }
-        }, {
-          key: "annotationQuery",
-          value: function annotationQuery(options) {
-            var query = this.templateSrv.replace(options.annotation.query, {}, 'glob');
-            var annotationQuery = {
-              range: options.range,
-              annotation: {
-                name: options.annotation.name,
-                datasource: options.annotation.datasource,
-                enable: options.annotation.enable,
-                iconColor: options.annotation.iconColor,
-                query: query
-              },
-              rangeRaw: options.rangeRaw
-            };
+            _export("GenericDatasource", GenericDatasource = function () {
+                function GenericDatasource(instanceSettings, $q, backendSrv, templateSrv) {
+                    _classCallCheck(this, GenericDatasource);
 
-            return this.doRequest({
-              url: this.url + '/annotations',
-              method: 'POST',
-              data: annotationQuery
-            }).then(function (result) {
-              return result.data;
-            });
-          }
-        }, {
-          key: "metricFindQuery",
-          value: function metricFindQuery(query, suburl) {
-            // var interpolated = {
-            //     target: this.templateSrv.replace(query, null, 'regex')
-            // };
+                    this.type = instanceSettings.type;
+                    this.url = instanceSettings.url;
+                    this.name = instanceSettings.name;
+                    this.q = $q;
+                    this.backendSrv = backendSrv;
+                    this.templateSrv = templateSrv;
+                    this.withCredentials = instanceSettings.withCredentials;
+                    this.headers = { 'Content-Type': 'application/json' };
+                    if (typeof instanceSettings.basicAuth === 'string' && instanceSettings.basicAuth.length > 0) {
+                        this.headers['Authorization'] = instanceSettings.basicAuth;
+                    }
+                }
 
-            return this.doRequest({
-              url: this.url + suburl,
-              // data: interpolated,
-              method: 'GET'
-            }).then(this.mapToTextValue);
-          }
-        }, {
-          key: "mapToTextValue",
-          value: function mapToTextValue(result) {
-            return _.map(result.data.value, function (data, index) {
-              return {
-                text: data.name + " ( " + data['@iot.id'] + " )",
-                value: data.name + " ( " + data['@iot.id'] + " )",
-                id: data['@iot.id']
-              };
-            });
-          }
-        }, {
-          key: "doRequest",
-          value: function doRequest(options) {
-            options.withCredentials = this.withCredentials;
-            options.headers = this.headers;
+                _createClass(GenericDatasource, [{
+                    key: "getTimeFilter",
+                    value: function getTimeFilter(options) {
+                        var from = options.range.from.format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z";
+                        var to = options.range.to.format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z";
+                        return "phenomenonTime gt " + from + " and phenomenonTime lt " + to;
+                    }
+                }, {
+                    key: "sleep",
+                    value: function sleep(delay) {
+                        var start = new Date().getTime();
+                        while (new Date().getTime() < start + delay) {};
+                    }
+                }, {
+                    key: "query",
+                    value: function query(options) {
 
-            return this.backendSrv.datasourceRequest(options);
-          }
-        }, {
-          key: "buildQueryParameters",
-          value: function buildQueryParameters(options) {
-            var _this = this;
+                        // Filter targets that are set to hidden
+                        options.targets = _.filter(options.targets, function (target) {
+                            return target.hide != true;
+                        });
 
-            //remove placeholder targets
-            options.targets = _.filter(options.targets, function (target) {
-              return target.dsTarget !== 'select metric';
-            });
+                        var allPromises = [];
+                        var allTargetResults = { data: [] };
+                        var self = this;
+                        var timeFilter = this.getTimeFilter(options);
 
-            var targets = _.map(options.targets, function (target) {
-              return {
-                target: _this.templateSrv.replace(target.dsTarget.toString(), options.scopedVars, 'regex'),
-                refId: target.refId,
-                hide: target.hide,
-                type: target.type || 'timeserie'
-              };
-            });
+                        // /Datastreams(16)/Observations?$filter=phenomenonTime%20gt%202018-03-14T16:00:12.749Z%20and%20phenomenonTime%20lt%202018-03-14T17:00:12.749Z&$select=result,phenomenonTime
 
-            options.targets = targets;
-            console.log(options);
-            return options;
-          }
-        }]);
+                        _.forEach(options.targets, function (target) {
+                            allPromises.push(this.doRequest({
+                                url: this.url + '/Datastreams(' + target.datastreamID + ')/Observations?' + '$filter=' + timeFilter,
+                                // data: query,
+                                method: 'GET'
+                            }).then(function (response) {
+                                var filtered = _.map(response.data.value, function (value, index) {
+                                    return [value.result, moment(value.resultTime, "YYYY-MM-DDTHH:mm:ss.SSSZ").format('x')];
+                                });
+                                return {
+                                    'target': target.dsTarget.toString(),
+                                    'datapoints': filtered
+                                };
+                            }));
+                        }.bind(this));
 
-        return GenericDatasource;
-      }());
+                        return Promise.all(allPromises).then(function (values) {
+                            _.forEach(values, function (value) {
+                                allTargetResults.data.push(value);
+                            });
+                            return allTargetResults;
+                        });
+                    }
+                }, {
+                    key: "testDatasource",
+                    value: function testDatasource() {
+                        return this.doRequest({
+                            url: this.url,
+                            method: 'GET'
+                        }).then(function (response) {
+                            if (response.status === 200) {
+                                return { status: "success", message: "Data source is working", title: "Success" };
+                            }
+                        });
+                    }
+                }, {
+                    key: "annotationQuery",
+                    value: function annotationQuery(options) {
+                        var query = this.templateSrv.replace(options.annotation.query, {}, 'glob');
+                        var annotationQuery = {
+                            range: options.range,
+                            annotation: {
+                                name: options.annotation.name,
+                                datasource: options.annotation.datasource,
+                                enable: options.annotation.enable,
+                                iconColor: options.annotation.iconColor,
+                                query: query
+                            },
+                            rangeRaw: options.rangeRaw
+                        };
 
-      _export("GenericDatasource", GenericDatasource);
-    }
-  };
+                        return this.doRequest({
+                            url: this.url + '/annotations',
+                            method: 'POST',
+                            data: annotationQuery
+                        }).then(function (result) {
+                            return result.data;
+                        });
+                    }
+                }, {
+                    key: "metricFindQuery",
+                    value: function metricFindQuery(query, suburl) {
+                        // var interpolated = {
+                        //     target: this.templateSrv.replace(query, null, 'regex')
+                        // };
+
+                        return this.doRequest({
+                            url: this.url + suburl,
+                            // data: interpolated,
+                            method: 'GET'
+                        }).then(this.mapToTextValue);
+                    }
+                }, {
+                    key: "mapToTextValue",
+                    value: function mapToTextValue(result) {
+                        return _.map(result.data.value, function (data, index) {
+                            return {
+                                text: data.name + " ( " + data['@iot.id'] + " )",
+                                value: data.name + " ( " + data['@iot.id'] + " )",
+                                id: data['@iot.id']
+                            };
+                        });
+                    }
+                }, {
+                    key: "doRequest",
+                    value: function doRequest(options) {
+                        options.withCredentials = this.withCredentials;
+                        options.headers = this.headers;
+
+                        return this.backendSrv.datasourceRequest(options);
+                    }
+                }, {
+                    key: "buildQueryParameters",
+                    value: function buildQueryParameters(options) {
+                        var _this = this;
+
+                        //remove placeholder targets
+                        options.targets = _.filter(options.targets, function (target) {
+                            return target.dsTarget !== 'select metric';
+                        });
+
+                        var targets = _.map(options.targets, function (target) {
+                            return {
+                                target: _this.templateSrv.replace(target.dsTarget.toString(), options.scopedVars, 'regex'),
+                                refId: target.refId,
+                                hide: target.hide,
+                                type: target.type || 'timeserie'
+                            };
+                        });
+
+                        options.targets = targets;
+                        console.log(options);
+                        return options;
+                    }
+                }]);
+
+                return GenericDatasource;
+            }());
+
+            _export("GenericDatasource", GenericDatasource);
+        }
+    };
 });
 //# sourceMappingURL=datasource.js.map
--- src/datasource.js
+++ src/datasource.js
@@ -2,146 +2,152 @@ import _ from "lodash";
 import moment from "moment";
 export class GenericDatasource {
 
-  constructor(instanceSettings, $q, backendSrv, templateSrv) {
+    constructor(instanceSettings, $q, backendSrv, templateSrv) {
 
-    this.type = instanceSettings.type;
-    this.url = instanceSettings.url;
-    this.name = instanceSettings.name;
-    this.q = $q;
-    this.backendSrv = backendSrv;
-    this.templateSrv = templateSrv;
-    this.withCredentials = instanceSettings.withCredentials;
-    this.headers = {'Content-Type': 'application/json'};
-    if (typeof instanceSettings.basicAuth === 'string' && instanceSettings.basicAuth.length > 0) {
-      this.headers['Authorization'] = instanceSettings.basicAuth;
+        this.type = instanceSettings.type;
+        this.url = instanceSettings.url;
+        this.name = instanceSettings.name;
+        this.q = $q;
+        this.backendSrv = backendSrv;
+        this.templateSrv = templateSrv;
+        this.withCredentials = instanceSettings.withCredentials;
+        this.headers = {'Content-Type': 'application/json'};
+        if (typeof instanceSettings.basicAuth === 'string' && instanceSettings.basicAuth.length > 0) {
+            this.headers['Authorization'] = instanceSettings.basicAuth;
+        }
     }
-  }
 
-  getTimeFilter(options){
-    let from = options.range.from.format("YYYY-MM-DDTHH:mm:ss.SSS")+"Z";
-    let to = options.range.to.format("YYYY-MM-DDTHH:mm:ss.SSS")+"Z";
-    return "phenomenonTime gt " + from + " and phenomenonTime lt " + to;
-  }
-  sleep(delay) {
-    var start = new Date().getTime();
-    while (new Date().getTime() < start + delay){
+    getTimeFilter(options){
+        let from = options.range.from.format("YYYY-MM-DDTHH:mm:ss.SSS")+"Z";
+        let to = options.range.to.format("YYYY-MM-DDTHH:mm:ss.SSS")+"Z";
+        return "phenomenonTime gt " + from + " and phenomenonTime lt " + to;
+    }
+    sleep(delay) {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay){
 
-    };
-  }
-  query(options) {
-    let allPromises = [];
-    let allTargetResults = {data:[]};
-    let self = this;
-    let timeFilter = this.getTimeFilter(options);
-
-    // /Datastreams(16)/Observations?$filter=phenomenonTime%20gt%202018-03-14T16:00:12.749Z%20and%20phenomenonTime%20lt%202018-03-14T17:00:12.749Z&$select=result,phenomenonTime
-
-    _.forEach(options.targets,function(target){
-      allPromises.push(this.doRequest({
-        url: this.url + '/Datastreams('+target.datastreamID+')/Observations?'+'$filter='+timeFilter,
-        // data: query,
-        method: 'GET'
-      }).then(function(response){
-        let filtered = _.map(response.data.value,function(value,index){
-          return [value.result,moment(value.resultTime,"YYYY-MM-DDTHH:mm:ss.SSSZ").format('x')];
-        });
-        return {
-          'target' : target.dsTarget.toString(),
-          'datapoints' : filtered
         };
-      }));
+    }
+    query(options) {
 
-    }.bind(this));
+        // Filter targets that are set to hidden
+        options.targets = _.filter(options.targets, target => {
+            return target.hide != true;
+        });
 
-    return Promise.all(allPromises).then(function(values) {
-      _.forEach(values,function(value){
-        allTargetResults.data.push(value);
-      });
-      return allTargetResults;
-    });
-  }
+        let allPromises = [];
+        let allTargetResults = {data:[]};
+        let self = this;
+        let timeFilter = this.getTimeFilter(options);
 
-  testDatasource() {
-    return this.doRequest({
-      url: this.url,
-      method: 'GET',
-    }).then(response => {
-      if (response.status === 200) {
-        return { status: "success", message: "Data source is working", title: "Success" };
-      }
-    });
-  }
+        // /Datastreams(16)/Observations?$filter=phenomenonTime%20gt%202018-03-14T16:00:12.749Z%20and%20phenomenonTime%20lt%202018-03-14T17:00:12.749Z&$select=result,phenomenonTime
 
-  annotationQuery(options) {
-    var query = this.templateSrv.replace(options.annotation.query, {}, 'glob');
-    var annotationQuery = {
-      range: options.range,
-      annotation: {
-        name: options.annotation.name,
-        datasource: options.annotation.datasource,
-        enable: options.annotation.enable,
-        iconColor: options.annotation.iconColor,
-        query: query
-      },
-      rangeRaw: options.rangeRaw
-    };
+        _.forEach(options.targets,function(target){
+            allPromises.push(this.doRequest({
+                url: this.url + '/Datastreams('+target.datastreamID+')/Observations?'+'$filter='+timeFilter,
+                // data: query,
+                method: 'GET'
+            }).then(function(response){
+                let filtered = _.map(response.data.value,function(value,index){
+                    return [value.result,moment(value.resultTime,"YYYY-MM-DDTHH:mm:ss.SSSZ").format('x')];
+                });
+                return {
+                    'target' : target.dsTarget.toString(),
+                    'datapoints' : filtered
+                };
+            }));
 
-    return this.doRequest({
-      url: this.url + '/annotations',
-      method: 'POST',
-      data: annotationQuery
-    }).then(result => {
-      return result.data;
-    });
-  }
+        }.bind(this));
 
-  metricFindQuery(query,suburl) {
-    // var interpolated = {
-    //     target: this.templateSrv.replace(query, null, 'regex')
-    // };
+        return Promise.all(allPromises).then(function(values) {
+            _.forEach(values,function(value){
+                allTargetResults.data.push(value);
+            });
+            return allTargetResults;
+        });
+    }
 
-    return this.doRequest({
-      url: this.url + suburl,
-      // data: interpolated,
-      method: 'GET',
-    }).then(this.mapToTextValue);
-  }
+    testDatasource() {
+        return this.doRequest({
+            url: this.url,
+            method: 'GET',
+        }).then(response => {
+            if (response.status === 200) {
+                return { status: "success", message: "Data source is working", title: "Success" };
+            }
+        });
+    }
 
-  mapToTextValue(result) {
-    return _.map(result.data.value, (data,index) => {
-      return {
-        text: data.name + " ( " + data['@iot.id'] + " )",
-        value: data.name + " ( " + data['@iot.id'] + " )",
-        id: data['@iot.id']
-      };
-    });
-  }
+    annotationQuery(options) {
+        var query = this.templateSrv.replace(options.annotation.query, {}, 'glob');
+        var annotationQuery = {
+            range: options.range,
+            annotation: {
+                name: options.annotation.name,
+                datasource: options.annotation.datasource,
+                enable: options.annotation.enable,
+                iconColor: options.annotation.iconColor,
+                query: query
+            },
+            rangeRaw: options.rangeRaw
+        };
 
-  doRequest(options) {
-    options.withCredentials = this.withCredentials;
-    options.headers = this.headers;
+        return this.doRequest({
+            url: this.url + '/annotations',
+            method: 'POST',
+            data: annotationQuery
+        }).then(result => {
+            return result.data;
+        });
+    }
 
-    return this.backendSrv.datasourceRequest(options);
+    metricFindQuery(query,suburl) {
+        // var interpolated = {
+        //     target: this.templateSrv.replace(query, null, 'regex')
+        // };
 
-  }
+        return this.doRequest({
+            url: this.url + suburl,
+            // data: interpolated,
+            method: 'GET',
+        }).then(this.mapToTextValue);
+    }
 
-  buildQueryParameters(options) {
-    //remove placeholder targets
-    options.targets = _.filter(options.targets, target => {
-      return target.dsTarget !== 'select metric';
-    });
+    mapToTextValue(result) {
+        return _.map(result.data.value, (data,index) => {
+            return {
+                text: data.name + " ( " + data['@iot.id'] + " )",
+                value: data.name + " ( " + data['@iot.id'] + " )",
+                id: data['@iot.id']
+            };
+        });
+    }
 
-    var targets = _.map(options.targets, target => {
-      return {
-        target: this.templateSrv.replace(target.dsTarget.toString(), options.scopedVars, 'regex') ,
-        refId: target.refId,
-        hide: target.hide,
-        type: target.type || 'timeserie'
-      };
-    });
+    doRequest(options) {
+        options.withCredentials = this.withCredentials;
+        options.headers = this.headers;
 
-    options.targets = targets;
-    console.log(options);
-    return options;
-  }
+        return this.backendSrv.datasourceRequest(options);
+
+    }
+
+    buildQueryParameters(options) {
+        //remove placeholder targets
+        options.targets = _.filter(options.targets, target => {
+            return target.dsTarget !== 'select metric';
+        });
+
+        var targets = _.map(options.targets, target => {
+            return {
+                target: this.templateSrv.replace(target.dsTarget.toString(), options.scopedVars, 'regex') ,
+                refId: target.refId,
+                hide: target.hide,
+                type: target.type || 'timeserie'
+            };
+        });
+
+        options.targets = targets;
+        console.log(options);
+        return options;
+    }
 }
