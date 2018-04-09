6337cebc87dadcde4e8feaae22eec7560225596d
rubanraj54 <rubanraj54@gmail.com>
2018-04-09 14:07:35 +0200

    added dropdown to show list of sensors, Showing list of datastreams based on the selected sensor

--- dist/query_ctrl.js
+++ dist/query_ctrl.js
@@ -1,128 +1,146 @@
 'use strict';
 
 System.register(['app/plugins/sdk', './css/query-editor.css!'], function (_export, _context) {
-  "use strict";
+    "use strict";
 
-  var QueryCtrl, _createClass, GenericDatasourceQueryCtrl;
+    var QueryCtrl, _createClass, GenericDatasourceQueryCtrl;
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  function _possibleConstructorReturn(self, call) {
-    if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
     }
 
-    return call && (typeof call === "object" || typeof call === "function") ? call : self;
-  }
-
-  function _inherits(subClass, superClass) {
-    if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-    }
-
-    subClass.prototype = Object.create(superClass && superClass.prototype, {
-      constructor: {
-        value: subClass,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-  }
-
-  return {
-    setters: [function (_appPluginsSdk) {
-      QueryCtrl = _appPluginsSdk.QueryCtrl;
-    }, function (_cssQueryEditorCss) {}],
-    execute: function () {
-      _createClass = function () {
-        function defineProperties(target, props) {
-          for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];
-            descriptor.enumerable = descriptor.enumerable || false;
-            descriptor.configurable = true;
-            if ("value" in descriptor) descriptor.writable = true;
-            Object.defineProperty(target, descriptor.key, descriptor);
-          }
+    function _possibleConstructorReturn(self, call) {
+        if (!self) {
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
         }
 
-        return function (Constructor, protoProps, staticProps) {
-          if (protoProps) defineProperties(Constructor.prototype, protoProps);
-          if (staticProps) defineProperties(Constructor, staticProps);
-          return Constructor;
-        };
-      }();
+        return call && (typeof call === "object" || typeof call === "function") ? call : self;
+    }
 
-      _export('GenericDatasourceQueryCtrl', GenericDatasourceQueryCtrl = function (_QueryCtrl) {
-        _inherits(GenericDatasourceQueryCtrl, _QueryCtrl);
-
-        function GenericDatasourceQueryCtrl($scope, $injector) {
-          _classCallCheck(this, GenericDatasourceQueryCtrl);
-
-          var _this = _possibleConstructorReturn(this, (GenericDatasourceQueryCtrl.__proto__ || Object.getPrototypeOf(GenericDatasourceQueryCtrl)).call(this, $scope, $injector));
-
-          _this.scope = $scope;
-          _this.target.dsTarget = _this.target.dsTarget || 'select metric';
-          _this.target.type = _this.target.type || 'timeserie';
-          _this.target.ogcType = _this.target.ogcType || "";
-          _this.target.ogcUrl = _this.target.ogcUrl || "";
-          _this.target.datastreamID = _this.target.datastreamID || 0;
-          _this.allDataSources = {};
-          return _this;
+    function _inherits(subClass, superClass) {
+        if (typeof superClass !== "function" && superClass !== null) {
+            throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
         }
 
-        _createClass(GenericDatasourceQueryCtrl, [{
-          key: 'sleep',
-          value: function sleep(delay) {
-            var start = new Date().getTime();
-            while (new Date().getTime() < start + delay) {};
-          }
-        }, {
-          key: 'getOptions',
-          value: function getOptions(query, ogcType) {
-
-            var metricTypes = {
-              'sensors': "/Sensors",
-              'datastreams': "/Datastreams"
-            };
-            var self = this;
-            this.target.ogcType = ogcType;
-            this.target.ogcUrl = metricTypes[ogcType];
-            return this.datasource.metricFindQuery(query || '', metricTypes[ogcType]).then(function (result) {
-              self.allDataSources = result;
-              return result;
-            });
-          }
-        }, {
-          key: 'toggleEditorMode',
-          value: function toggleEditorMode() {
-            this.target.rawQuery = !this.target.rawQuery;
-          }
-        }, {
-          key: 'onChangeInternal',
-          value: function onChangeInternal(query) {
-            var selectedDataSource = _.find(this.allDataSources, { 'value': this.target.dsTarget });
-            if (selectedDataSource) {
-              this.target.datastreamID = selectedDataSource.id;
-            } else {
-              this.target.datastreamID = 0;
+        subClass.prototype = Object.create(superClass && superClass.prototype, {
+            constructor: {
+                value: subClass,
+                enumerable: false,
+                writable: true,
+                configurable: true
             }
-            this.panelCtrl.refresh();
-          }
-        }]);
-
-        return GenericDatasourceQueryCtrl;
-      }(QueryCtrl));
-
-      _export('GenericDatasourceQueryCtrl', GenericDatasourceQueryCtrl);
-
-      GenericDatasourceQueryCtrl.templateUrl = 'partials/query.editor.html';
+        });
+        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
     }
-  };
+
+    return {
+        setters: [function (_appPluginsSdk) {
+            QueryCtrl = _appPluginsSdk.QueryCtrl;
+        }, function (_cssQueryEditorCss) {}],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
+
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
+                };
+            }();
+
+            _export('GenericDatasourceQueryCtrl', GenericDatasourceQueryCtrl = function (_QueryCtrl) {
+                _inherits(GenericDatasourceQueryCtrl, _QueryCtrl);
+
+                function GenericDatasourceQueryCtrl($scope, $injector) {
+                    _classCallCheck(this, GenericDatasourceQueryCtrl);
+
+                    var _this = _possibleConstructorReturn(this, (GenericDatasourceQueryCtrl.__proto__ || Object.getPrototypeOf(GenericDatasourceQueryCtrl)).call(this, $scope, $injector));
+
+                    _this.scope = $scope;
+
+                    _this.target.type = _this.target.type || 'timeserie';
+
+                    _this.target.senTarget = _this.target.senTarget || 'select sensor';
+                    _this.allSensors = {};
+                    _this.target.selectedSensorId = _this.target.selectedSensorId || 0;
+
+                    _this.target.dsTarget = _this.target.dsTarget || 'select metric';
+                    _this.target.datastreamID = _this.target.datastreamID || 0;
+                    _this.allDataSources = {};
+                    return _this;
+                }
+
+                _createClass(GenericDatasourceQueryCtrl, [{
+                    key: 'sleep',
+                    value: function sleep(delay) {
+                        var start = new Date().getTime();
+                        while (new Date().getTime() < start + delay) {};
+                    }
+                }, {
+                    key: 'getOptions',
+                    value: function getOptions(query) {
+                        var self = this;
+                        return this.datasource.metricFindQuery(query || '', "/Sensors(" + this.target.selectedSensorId + ")/Datastreams").then(function (result) {
+                            self.allDataSources = result;
+                            return result;
+                        });
+                    }
+                }, {
+                    key: 'getSensors',
+                    value: function getSensors(query) {
+                        var self = this;
+                        return this.datasource.metricFindQuery(query || '', "/Sensors").then(function (result) {
+                            self.allSensors = result;
+                            return result;
+                        });
+                    }
+                }, {
+                    key: 'toggleEditorMode',
+                    value: function toggleEditorMode() {
+                        this.target.rawQuery = !this.target.rawQuery;
+                    }
+                }, {
+                    key: 'onChangeInternal',
+                    value: function onChangeInternal(query) {
+                        var selectedDataSource = _.find(this.allDataSources, { 'value': this.target.dsTarget });
+                        if (selectedDataSource) {
+                            this.target.datastreamID = selectedDataSource.id;
+                        } else {
+                            this.target.datastreamID = 0;
+                        }
+                        this.panelCtrl.refresh();
+                    }
+                }, {
+                    key: 'onSensorChange',
+                    value: function onSensorChange(query) {
+                        this.target.dsTarget = "select metric";
+                        var selectedSensor = _.find(this.allSensors, { 'value': this.target.senTarget });
+                        if (selectedSensor) {
+                            this.target.selectedSensorId = selectedSensor.id;
+                        } else {
+                            this.target.selectedSensorId = 0;
+                        }
+                        this.onChangeInternal();
+                    }
+                }]);
+
+                return GenericDatasourceQueryCtrl;
+            }(QueryCtrl));
+
+            _export('GenericDatasourceQueryCtrl', GenericDatasourceQueryCtrl);
+
+            GenericDatasourceQueryCtrl.templateUrl = 'partials/query.editor.html';
+        }
+    };
 });
 //# sourceMappingURL=query_ctrl.js.map
--- src/query_ctrl.js
+++ src/query_ctrl.js
@@ -2,55 +2,70 @@ import {QueryCtrl} from 'app/plugins/sdk';
 import './css/query-editor.css!'
 export class GenericDatasourceQueryCtrl extends QueryCtrl {
 
-  constructor($scope, $injector)  {
-    super($scope, $injector);
+    constructor($scope, $injector)  {
+        super($scope, $injector);
 
-    this.scope = $scope;
-    this.target.dsTarget = this.target.dsTarget || 'select metric';
-    this.target.type = this.target.type || 'timeserie';
-    this.target.ogcType = this.target.ogcType || "";
-    this.target.ogcUrl = this.target.ogcUrl || "";
-    this.target.datastreamID = this.target.datastreamID || 0;
-    this.allDataSources  = {};
-  }
+        this.scope = $scope;
 
-  sleep(delay) {
-    var start = new Date().getTime();
-    while (new Date().getTime() < start + delay){
+        this.target.type = this.target.type || 'timeserie';
 
-    };
-  }
+        this.target.senTarget = this.target.senTarget || 'select sensor';
+        this.allSensors  = {};
+        this.target.selectedSensorId = this.target.selectedSensorId || 0;
 
-
-  getOptions(query,ogcType) {
-
-    let metricTypes = {
-      'sensors' : "/Sensors",
-      'datastreams' : "/Datastreams",
-    };
-    let self = this;
-    this.target.ogcType = ogcType;
-    this.target.ogcUrl = metricTypes[ogcType];
-    return this.datasource.metricFindQuery((query || ''),metricTypes[ogcType]).then((result)=>{
-      self.allDataSources = result;
-      return result;
-    });
-
-  }
-
-  toggleEditorMode() {
-    this.target.rawQuery = !this.target.rawQuery;
-  }
-
-  onChangeInternal(query) {
-    let selectedDataSource =_.find(this.allDataSources, { 'value' : this.target.dsTarget });
-    if (selectedDataSource) {
-        this.target.datastreamID = selectedDataSource.id ;
-    } else {
-        this.target.datastreamID = 0 ;
+        this.target.dsTarget = this.target.dsTarget || 'select metric';
+        this.target.datastreamID = this.target.datastreamID || 0;
+        this.allDataSources  = {};
     }
-    this.panelCtrl.refresh();
-  }
+
+    sleep(delay) {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay){
+
+        };
+    }
+
+
+    getOptions(query) {
+        let self = this;
+        return this.datasource.metricFindQuery((query || ''),"/Sensors("+this.target.selectedSensorId+")/Datastreams").then((result)=>{
+            self.allDataSources = result;
+            return result;
+        });
+    }
+
+    getSensors(query) {
+        let self = this;
+        return this.datasource.metricFindQuery((query || ''),"/Sensors").then((result)=>{
+            self.allSensors = result;
+            return result;
+        });
+    }
+
+    toggleEditorMode() {
+        this.target.rawQuery = !this.target.rawQuery;
+    }
+
+    onChangeInternal(query) {
+        let selectedDataSource =_.find(this.allDataSources, { 'value' : this.target.dsTarget });
+        if (selectedDataSource) {
+            this.target.datastreamID = selectedDataSource.id ;
+        } else {
+            this.target.datastreamID = 0 ;
+        }
+        this.panelCtrl.refresh();
+    }
+
+    onSensorChange(query) {
+        this.target.dsTarget = "select metric";
+        let selectedSensor =_.find(this.allSensors, { 'value' : this.target.senTarget });
+        if (selectedSensor) {
+            this.target.selectedSensorId = selectedSensor.id ;
+        } else {
+            this.target.selectedSensorId = 0 ;
+        }
+        this.onChangeInternal();
+    }
 }
 
 GenericDatasourceQueryCtrl.templateUrl = 'partials/query.editor.html';
